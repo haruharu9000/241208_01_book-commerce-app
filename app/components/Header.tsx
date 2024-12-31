ece04188a121e0a696f1390877d6cfe1187fadff
haruharu9000 <nitori4baime@gmail.com>
2024-12-31 18:36:46 +0900

    commit

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { callbackify } from "util";
 
 const Header = () => {
   const { data: session } = useSession();
@@ -23,11 +24,21 @@ const Header = () => {
             ホーム
           </Link>
           <Link
-            href="/login"
+            href={user ? "/profile" : "/login"}
             className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
           >
-            ログイン
+            {user ? "プロフィール" : "ログイン"}
           </Link>
+
+          {user ? (
+            <button onClick={() => signOut({ callbackUrl: "/login" })}
+            className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+          >
+              ログアウト
+            </button>
+          ) : (
+          ""
+          )}
 
           <Link href={`/profile`}>
             <Image
