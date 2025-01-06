4d51c130ea3eb213248e00ade521ebda163ebc46
haruharu9000 <nitori4baime@gmail.com>
2025-01-06 21:44:47 +0900

    commit

--- .zshrc
+++ .zshrc
@@ -50,3 +50,4 @@ export PATH=/opt/homebrew/bin:$PATH
 export PATH=/opt/homebrew/bin:$PATH
 export PATH=/opt/homebrew/bin:$PATH
 export PATH=/opt/homebrew/bin:$PATH
+export PATH=/opt/homebrew/bin:$PATH
--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -1,15 +1,16 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
+//https://nextjs.org/docs/app/building-your-application/routing/route-handlers#dynamic-route-segments
 export async function GET(
-  req: Request,
+  request: Request,
   { params }: { params: { userId: string } }
 ) {
   const { userId } = params;
 
   try {
     const purchase = await prisma.purchase.findMany({
-      where: { userId },
+      where: { userId: userId  },
     });
     console.log(purchase);
 
