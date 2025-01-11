930515babb502627c933718c5bc47d837f4fd488
haruharu9000 <nitori4baime@gmail.com>
2025-01-11 18:08:39 +0900

    commit

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -1,16 +1,12 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
-//https://nextjs.org/docs/app/building-your-application/routing/route-handlers#dynamic-route-segments
-export async function GET(
-  request: Request,
-  { params }: { params: { userId: string } }
-) {
+export async function GET(request: Request, { params }: { params: { userId: string } }) {
   const userId = params.userId;
 
   try {
     const purchases = await prisma.purchase.findMany({
-      where: { userId: userId },
+      where: { userId },
     });
     console.log(purchases);
 
