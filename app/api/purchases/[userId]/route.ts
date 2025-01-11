823946ad26e61a8bcdf370b435c84e898aac236b
haruharu9000 <nitori4baime@gmail.com>
2025-01-11 18:24:17 +0900

    commit

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -1,21 +1,21 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, { params }: { params: { userId: string } }) {
+//https://nextjs.org/docs/app/building-your-application/routing/route-handlers#dynamic-route-segments
+export async function GET(
+  request: Request,
+  { params }: { params: { userId: string } }
+) {
   const userId = params.userId;
 
   try {
-    const purchases = await prisma.purchase.findMany({
-      where: { userId },
+    const purchase = await prisma.purchase.findMany({
+      where: { userId: userId },
     });
-    console.log(purchases);
+    console.log(purchase);
 
-    return NextResponse.json(purchases);
+    return NextResponse.json(purchase);
   } catch (err) {
-    console.error("Failed to fetch purchases:", err);
-    return NextResponse.json(
-      { error: "Failed to fetch purchases" },
-      { status: 500 }
-    );
+    return NextResponse.json(err);
   }
 }
