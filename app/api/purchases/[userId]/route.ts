b0fae1a5f770e1ca0234a71859b8c799f358825d
haruharu9000 <nitori4baime@gmail.com>
2025-01-11 18:55:18 +0900

    commit

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -1,17 +1,19 @@
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
-// GET ハンドラの定義
 export async function GET(
   request: Request,
-  context: { params: { userId: string } } // 型定義を修正
+  { params }: { params: { userId: string } }
 ) {
-  const userId = context.params.userId;
+  console.log("Params received:", params); // デバッグログ追加
+
+  const userId = params.userId;
 
   try {
     const purchases = await prisma.purchase.findMany({
       where: { userId },
     });
+    console.log("Purchases found:", purchases); // デバッグログ追加
 
     return NextResponse.json(purchases);
   } catch (err) {
