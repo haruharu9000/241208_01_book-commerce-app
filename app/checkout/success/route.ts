cfe42ff2c00d79260c375b618809b03fb5c3ae20
haruharu9000 <nitori4baime@gmail.com>
2025-01-17 22:00:09 +0900

    commit

--- app/checkout/success/route.ts
+++ app/checkout/success/route.ts
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-//購入履歴保存
+//購入履歴の保存
 export async function POST(request: Request) {
     const { sessionId } = await request.json();
 
