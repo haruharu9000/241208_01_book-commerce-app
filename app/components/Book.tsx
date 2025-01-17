ca72b1c95133ef32fd3d558b0e3f2b8f0eef6dc5
haruharu9000 <nitori4baime@gmail.com>
2025-01-17 21:51:44 +0900

    commit

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -68,7 +68,7 @@ const Book = ({ book, isPurchased }: BookProps) => {
     if (!user) {
       setShowModal(false);
       // ログインページへリダイレクト
-      router.push("/api/auth/signin")
+      router.push("/api/auth/signin");
     } else {
       // Stripeで決済する
       startCheckout();
