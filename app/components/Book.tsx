bb4836a09c3f1317a0ae9766f73faa85302b92f6
haruharu9000 <nitori4baime@gmail.com>
2025-02-01 18:51:51 +0900

    commit

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -11,7 +11,6 @@ type BookProps = {
   isPurchased: boolean;
 };
 
-// eslint-disable-next-line react/display-name
 const Book = ({ book, isPurchased }: BookProps) => {
   console.log("Book props:", book);
 
@@ -20,35 +19,34 @@ const Book = ({ book, isPurchased }: BookProps) => {
   const user = session?.user as {
     id: string;
     name?: string;
-    email?: string
+    email?: string;
   };
   const router = useRouter();
-  console.log(user?.id)
+  console.log(user?.id);
   console.log(book.id);
 
   const startCheckout = async () => {
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            title: book.title,
-            price: book.price,
-            userId: user?.id,
-            bookId: book.id,
-          }),
-        }
-      );
+      const response = await fetch("/api/checkout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: book.title,
+          price: book.price,
+          bookId: book.id,
+          userId: user?.id,
+        }),
+      });
 
       const responseData = await response.json();
 
-      if (responseData) {
-        router.push(responseData.checkout_url);
+      if (responseData.checkout_url) {
+        window.location.href = responseData.checkout_url;
       }
-    } catch (err) {
-      console.error("Error in startCheckout:", err);
+    } catch (error) {
+      console.error("Error in startCheckout:", error);
     }
   };
 
@@ -56,7 +54,7 @@ const Book = ({ book, isPurchased }: BookProps) => {
     if (isPurchased) {
       alert("その商品は購入済みです。");
     } else {
-       setShowModal(true);
+      setShowModal(true);
     }
   };
 
@@ -73,7 +71,7 @@ const Book = ({ book, isPurchased }: BookProps) => {
       // Stripeで決済する
       startCheckout();
     }
-  }
+  };
 
   return (
     <>
@@ -97,7 +95,8 @@ const Book = ({ book, isPurchased }: BookProps) => {
       <div className="flex flex-col items-center m-4">
         <a
           onClick={handlePurchaseClick}
-          className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
+          className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none"
+        >
           <Image
             priority
             src={book.thumbnail.url}
@@ -115,18 +114,22 @@ const Book = ({ book, isPurchased }: BookProps) => {
 
         {showModal && (
           <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
-          <div className="bg-white p-8 rounded-lg">
-            <h3 className="text-xl mb-4">本を購入しますか？</h3>
-            <button onClick={handlePurchaseConfirm} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
-              購入する
-            </button>
-            <button
-              onClick={handleCancel}
-              className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-              キャンセル
-            </button>
+            <div className="bg-white p-8 rounded-lg">
+              <h3 className="text-xl mb-4">本を購入しますか？</h3>
+              <button
+                onClick={handlePurchaseConfirm}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+              >
+                購入する
+              </button>
+              <button
+                onClick={handleCancel}
+                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+              >
+                キャンセル
+              </button>
+            </div>
           </div>
-        </div>
         )}
       </div>
     </>
