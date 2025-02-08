4ccb0ba50956e18c1b1c166bb918d45c7ee3bb88
haruharu9000 <nitori4baime@gmail.com>
2025-02-08 17:30:31 +0900

    fix: update Stripe env variables

--- next.config.js
+++ next.config.js
@@ -4,10 +4,23 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
+        port: '',
+        pathname: '/**', // すべてのパスを許可
+        search: '',
       },
       {
         protocol: "https",
         hostname: "images.microcms-assets.io",
+        port: '',
+        pathname: '/**', // すべてのパスを許可
+        search: '',
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // Googleのプロフィール画像
+        port: '',
+        pathname: '/**', // すべてのパスを許可
+        search: '',
       },
     ],
   },
