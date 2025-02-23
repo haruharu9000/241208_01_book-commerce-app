8efb386227efd1c0979c3f6a67893ac3bd84d094
haruharu9000 <nitori4baime@gmail.com>
2025-02-23 14:49:33 +0900

    Add subtitles for book thumbnails

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -103,7 +103,7 @@ const Book = ({ book, isPurchased, user }: BookProps) => {
           />
           <div className="px-4 py-4 bg-slate-100 rounded-b-md">
             <h2 className="text-lg font-semibold">{book.title}</h2>
-            <p className="mt-2 text-lg text-slate-600">{book.content}</p>
+            <p className="mt-2 text-lg text-slate-600">{book.description}</p>
             <p className="mt-2 text-md text-slate-700">価格:{book.price}円</p>
           </div>
         </a>
