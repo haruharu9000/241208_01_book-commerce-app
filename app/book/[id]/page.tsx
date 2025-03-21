9a186293d481918092ca7b8753a4e6f4718ed585
haruharu9000 <nitori4baime@gmail.com>
2025-03-21 23:49:53 +0900

    commit

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -109,8 +109,13 @@ const DetailBook = async ({ params }: { params: { id: string } }) => {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            // セッションCookieを含める
+            Cookie: (await getServerSession(nextAuthOptions))?.user
+              ? `next-auth.session-token=${(await getServerSession(nextAuthOptions))?.user}`
+              : "",
           },
           credentials: "include",
+          cache: "no-store",
           body: JSON.stringify({
             bookId: book.id,
             userId: user.id,
