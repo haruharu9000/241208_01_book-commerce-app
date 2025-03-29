8227d8c8d851411e9e240a55194e76ef506c3c02
haruharu9000 <nitori4baime@gmail.com>
2025-03-29 14:15:36 +0900

    commit

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,20 +1,22 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
-import { useSession, signOut } from "next-auth/react";
+import React from "react";
+import { getServerSession } from "next-auth";
+import { nextAuthOptions } from "../lib/next-auth/options";
+import { User } from "../types/types";
 
-const Header = () => {
-  const { data: session } = useSession();
+const Header = async () => {
+  const session = await getServerSession(nextAuthOptions);
+  const user = session?.user as User;
 
   const handleLogout = async () => {
-    await signOut({ callbackUrl: "/" });
+    window.location.href = "/api/auth/signout";
   };
 
   return (
     <header className="bg-slate-600 text-gray-100 shadow-lg">
       <nav className="flex items-center justify-between p-4">
-        <Link href="/" className="text-xl font-bold">
+        <Link href={"/"} className="text-xl font-bold">
           Medium
         </Link>
         <div className="flex items-center gap-1">
@@ -24,17 +26,23 @@ const Header = () => {
           >
             ホーム
           </Link>
-          {session ? (
+          {user ? (
             <>
+              <Link
+                href="/profile"
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                プロフィール
+              </Link>
               <button
                 onClick={handleLogout}
-                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer"
+                className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
               >
                 ログアウト
               </button>
               <Link href="/profile">
                 <Image
-                  src={session.user?.image || "/default-avatar.png"}
+                  src={user?.image || "/default-avatar.png"}
                   alt="プロフィール"
                   width={40}
                   height={40}
